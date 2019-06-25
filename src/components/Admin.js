7a4f8df1e7bd108e61c88761fce9549e3350f1e8
Mustapha Kadri <mustapha.kadri66@gmail.com>
2019-06-25 19:00:02 +0200

    feat(auth) - auth with facebook

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -2,11 +2,57 @@ import React, { Component } from 'react'
 
 import AjouterRecette from './AjouterRecette'
 import AdminForm from './AdminForm'
+import Login from './Login'
+
+// import Firebase tools
+import firebase from 'firebase/app' 
+import  'firebase/auth' 
+import base , { firebaseApp } from '../base'
 
 class Admin extends Component {
+    state = {
+        uid: null,
+        chef: null
+    }
+
+    handleAuth = async (authData) => {
+        const box = await base.fetch(this.props.pseudo, {context: this})
+
+        if(!box.chef){
+            await base.post(`${this.props.pseudo}/chef`,{
+                data: authData.user.uid
+            })
+        }
+
+        this.setState({
+            uid: authData.user.uid,
+            chef: box.chef || authData.user.uid
+        })
+    }
+
+    authenticate = () => {
+        const authProvider = new firebase.auth.FacebookAuthProvider()
+        firebaseApp
+            .auth()
+            .signInWithPopup(authProvider)
+            .then(this.handleAuth)
+    }
 
     render() {
         const {recettes, ajouterRecette, majRecette, supprimerRecette, chargerExemple} = this.props
+
+        // si l'utilisateur n'est pas connecté
+        if(!this.state.uid){
+            return <Login authenticate={this.authenticate}></Login>
+        }
+
+        if(this.state.uid !== this.state.chef){
+            return(
+                <div>
+                    Tu n'es pas le chef de cette boite !
+                </div>
+            )
+        }
         return (
             <div className="cards">
                 <AjouterRecette ajouterRecette={ajouterRecette}/>
---
+++ src/components/Login.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Login  = ({authenticate}) => {
+    return (
+        <div className="login">
+            <h2>Connecte pour créer tes recettes !</h2>
+            <button 
+                onClick={authenticate}
+                className="facebook-button">
+                Me connecter avec facebook
+            </button>
+        </div>
+    )
+
+}
+
+export default Login
